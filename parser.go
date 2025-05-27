dbdf662dd1c54a2e8ced7b9ebac608d76733700a
Sushant <sushant.dhiman9812@gmail.com>
2025-05-27 07:28:23 +0530

    implemented a simple parse value function

--- parser.go
+++ parser.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+)
+
 type ASTNode interface {
 	Type() string
 }
@@ -48,4 +53,32 @@ type NullNode struct{}
 
 func (n NullNode) Type() string {
 	return "Null"
+}
+
+func Parser(tokens []Token) (ASTNode, error) {
+	if len(tokens) == 0 {
+		return nil, errors.New("nothing to parse")
+	}
+
+	return nil, nil
+}
+
+func parseValue(token Token) (ASTNode, error) {
+	switch token.Type {
+	case TKN_STRING:
+		return StringNode{Value: token.Value}, nil
+
+	case TKN_NUMBER:
+		num, _ := strconv.ParseFloat(token.Value, 64)
+		return NumberNode{Value: num}, nil
+
+	case TKN_TRUE:
+		return BooleanNode{Value: true}, nil
+	case TKN_FALSE:
+		return BooleanNode{Value: false}, nil
+	case TKN_NULL:
+		return NullNode{}, nil
+	default:
+		return nil, errors.New("invalid token type: " + token.Type)
+	}
 }

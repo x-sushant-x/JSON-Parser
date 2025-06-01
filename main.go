6c4e430295d356e58a4d330f313924a7b38d179e
Sushant <sushant.dhiman9812@gmail.com>
2025-06-01 10:15:17 +0530

    Simple JSON used for testing

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func main() {
-	input := `{ "user": { "id": 1024, "name": "Sushant \"The Dev\"", "bio": "Backend Engineer", "active": true, "roles": ["admin", "moderator", "editor"], "preferences": { "notifications": { "email": true, "sms": false }, "theme": null } }, "projects": [ { "id": "p001", "name": "RateShield", "techStack": ["Go", "Redis", "REST"], "contributors": 3 }, { "id": "p002", "name": "LogPump", "techStack": ["Rust", "Elasticsearch", "TCP"], "contributors": 2 } ], "lastLogin": "2025-05-25T14:30:00Z", "metadata": null }`
+	input := `{
+		"id": "d5475d58-9a02-49ae-ba2e-93d6a6c2e39e",
+		"name": "iPhone 6s",
+		"price": 649.99,
+		"isAvailable": true,
+		"colors" : ["White", "Grey"],
+		"discount" : null
+	}`
 
 	tokens, err := Tokenize(input)
 
